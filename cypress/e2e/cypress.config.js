cf4e76df39e3fe5086362c3e3c33567bbb389a03
Kauan de Oliveira <okauan652@gmail.com>
2026-08-21 02:24:34 -0300

    Configure cypress cloud

--- cypress/e2e/cypress.config.js
+++ cypress/e2e/cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "p3zbsz",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    projectId: "p3zbsz",
+    
   },
 });
