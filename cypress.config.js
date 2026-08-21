ace50747b450a1fa8c9deebd33ea104b12a89c71
Kauan de Oliveira <okauan652@gmail.com>
2026-08-21 02:43:20 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "p3zbsz",
   viewportHeight: 1000,
   viewportWidth: 600,
   e2e: {},
--- cypress/e2e/cypress.config.js
+++ cypress/e2e/cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "p3zbsz",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
